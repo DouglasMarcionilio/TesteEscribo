ec73af8020126b61e8f5c54db897d511123cacb9
Douglas Marcionilio Ferreira Aguiar <52482060+DouglasMarcionilio@users.noreply.github.com>
2022-01-19 11:28:44 -0300

    Update main.dart
    Adição do controle do numero digitado e comentários

--- main.dart
+++ main.dart
@@ -3,16 +3,26 @@ import 'dart:convert';
 import 'dart:core';
 
 void main(){
+  //Solicitando e lendo o numero digitado
   print('Insira um numero inteiro: ');
   int insertedNum = int.parse(stdin.readLineSync());
+  
+  //Verificando se o numero digitado é um numero positivo
+  while(insertedNum < 0){
+    print('Numero Invalido! Insira um numero inteiro maior que 0: ');
+    insertedNum = int.parse(stdin.readLineSync());
+  }
+  
   int result = 0;
   
+  //Verificando se os numeros menores que o digitado são divisiveis por 3 ou 5
   for(int i = 1; i < insertedNum; i++){
     if((i % 3 == 0) || (i % 5 == 0)){
+      //Somando os numeros divisiveis
       result += i;
     }
   }
-
-  print('Soma dos divisíveis: $result');
   
+  //Mostrando o resultado da soma
+  print('Soma dos divisíveis: $result'); 
 }
